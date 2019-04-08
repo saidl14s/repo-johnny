21a988affa7c7891ba2c5cfb86bd8991f12d9011
Jesús Said Llamas Manriquez <saidllamas@MacBook-Air-de-Jesus.local>
2019-04-08 07:46:10 -0500

    resolve

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3002
 
 var five = require("johnny-five");
 var board = new five.Board();
 
 
 app.get('/', function(req, res) {
+    var led = new five.Led({ pin: 13, board: board });
+    //var led = new five.Led(13);
+    led.off();
     res.sendfile('index.html');
 });
 
-app.get('/change', function(req, res) {
-    board.on("ready", function() {
-        //var led = new five.Led({ pin: 13, board: board });
-        var led = new five.Led(13);
+app.get('/off', function(req, res) {
+    /*board.on("ready", function() {
+        var led = new five.Led({ pin: 13, board: board });
+        //var led = new five.Led(13);
         led.toggle();
         res.redirect('/');
-    });
+    });*/
+    var led = new five.Led({ pin: 13, board: board });
+    //var led = new five.Led(13);
+    led.on();
+    res.sendfile('off.html');
 
 });
 
